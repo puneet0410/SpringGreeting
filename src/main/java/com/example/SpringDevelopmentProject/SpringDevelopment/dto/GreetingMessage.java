08787132116a4584c5f39ad50e3f0514b6897a86
puneet0410 <puneetp2200@gmail.com>
2025-03-03 23:04:03 +0530

    UC_1

--- src/main/java/com/example/SpringDevelopmentProject/SpringDevelopment/dto/GreetingMessage.java
+++ src/main/java/com/example/SpringDevelopmentProject/SpringDevelopment/dto/GreetingMessage.java
@@ -1,5 +1,7 @@
 package com.example.SpringDevelopmentProject.SpringDevelopment.dto;
 
+import java.sql.SQLOutput;
+
 public class GreetingMessage {
     private String greetMessage;
 public GreetingMessage(String greetMessage){
@@ -9,6 +11,7 @@ public GreetingMessage(){
 
 }
 public String getGreetMessage(){
+//    System.out.println("starting message");
     return greetMessage;
 }
 public void Setmessage(String greetMessage){
